c93bc0c5026619c5f152f3667e52610a4a178d9b
hira0448 <99321564+hira0448@users.noreply.github.com>
2022-04-07 06:17:49 +0900

    const変数での変数宣言 const変数は上書きも再宣言も不可能

--- src/index.js
+++ src/index.js
@@ -13,12 +13,21 @@
 // var val1 = "var変数を再宣言";
 // console.log(val1);
 
-let val2 = "let変数";
-console.log(val2);
+// let val2 = "let変数";
+// console.log(val2);
 
-// letは上書きが可能
-val2 = "let変数を上書き";
-console.log(val2);
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
 
-// letは再宣言不可能
-let val2 = "let変数を再宣言";
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+const val3 = "const変数";
+console.log(val3);
+
+// const変数は上書き不可能
+val3 = "const変数を上書き";
+
+// const変数は再宣言不可能
+const val3 = "const変数を再宣言";
